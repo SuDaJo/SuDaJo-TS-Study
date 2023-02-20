678c2389f4e6399799d7bf3bdf04dd82db3e58e4
DADAKIM <d.ya96v@gmail.com>
2023-02-20 21:57:46 +0900

    230220_타입 변수지정 Type alias & 읽기전용속성 readonly

---
+++ DA/230220_Narrowing&type_alias/230220_typeAlias&readOnly.js
@@ -0,0 +1,25 @@
+let animal = 123;
+let animalObj = { name: "kim", age: 28 };
+// const의 특징 : 재할당은 금지! but, const로 담은 Object 수정을 막는 것은 아님
+const 출생지역 = { region: "daejeon" };
+출생지역.region = "seoul";
+const friend = {
+    name: "dada"
+};
+let position = {
+    x: 10,
+    y: 20
+};
+let multiTest = {
+    size: 123,
+    position: [1, 2, 3]
+};
+let joinInfo = {
+    name: 'kim',
+    phone: 123,
+};
+let newJoinInfo = {
+    name: 'kim',
+    isAdult: false,
+    phone: 1234
+};
